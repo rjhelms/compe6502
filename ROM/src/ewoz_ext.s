1273f712e81d5f4af46c42a18cf26691b210905d
Rob Hailman <robhailman@gmail.com>
2021-12-24 16:24:07 -0500

    Improve PRSTATUS display
    Make BRK output easier to parse

--- ROM/src/ewoz_ext.s
+++ ROM/src/ewoz_ext.s
@@ -11,20 +11,42 @@ SREG:   .res 1
 PCL:    .res 1
 PCH:    .res 1
 
+.macro show_register reg
+.scope
+        @val := .ident(.sprintf("MSG_%s", .string(reg)))
+        lda #<@val
+        sta MSGL
+        lda #>@val
+        sta MSGH
+        jsr SHWMSG
+        lda reg
+        jsr PRBYTE
+.endscope
+.endmacro
+
 .segment "MON"
 
 .proc PRSTATUS
         lda #$0D
         jsr COUT
         jsr BEEP
-        ldy #$07        ; output in reverse order
-:       lda AREG, Y
+        lda PCH
         jsr PRBYTE
-        lda #$20
-        jsr COUT
-        dey
-        bne :-
+        lda PCL
+        jsr PRBYTE
+        show_register AREG
+        show_register XREG
+        show_register YREG
+        show_register SREG
+        show_register PREG
         lda #$0D
         jsr COUT
         rts
 .endproc
+
+.segment "RODATA"
+MSG_AREG:       .byte " A=",0
+MSG_XREG:       .byte " X=",0
+MSG_YREG:       .byte " Y=",0
+MSG_SREG:       .byte " S=",0
+MSG_PREG:       .byte " P=",0
