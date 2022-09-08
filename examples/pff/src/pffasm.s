2e5eae7b88b716b37f705ca675c69123561eb90a
Rob Hailman <robhailman@gmail.com>
2022-09-08 00:22:56 -0400

    Port pf_mount to assembly
    One method to go!

--- examples/pff/src/pffasm.s
+++ examples/pff/src/pffasm.s
@@ -5,6 +5,13 @@
     rol addr+1
 .endmacro
 
+.macro shift_right_32 addr
+    lsr addr+3
+    ror addr+2
+    ror addr+1
+    ror addr
+.endmacro
+
 .macro print_a
     pha
     pha
@@ -106,12 +113,25 @@ AM_ARC  = $20   ; archive
 AM_MASK = $3F   ; mask of defined bits
 
 ; boot sector offsets
+BPB_SecPerClus = 13
+BPB_RsvdSecCnt = 14
+BPB_NumFATs = 16
+BPB_RootEntCnt = 17
+BPB_TotSec16 = 19
+BPB_FATSz16 = 22
+BPB_TotSec32 = 32
+BPB_FATSz32 = 36
 BS_FilSysType = 54
+MBR_Table = 446
 
 ; directory entry offsets
 DIR_Attr = 11
 DIR_FstClusLO = 26
 DIR_FileSize = 28
+
+; DSTATUS flags
+STA_NOINIT  = $01   ; disk not initialized
+STA_NODISK  = $02   ; disk no present
 
 .global _FatFs
 .global _dj
@@ -127,9 +147,11 @@ DIR_FileSize = 28
 .global _sector
 .global _result
 
+.import _disk_initialize
 .import _disk_readp
 .import _work
 
+.export _pf_mount
 .export _pf_open
 
 .export _check_fs
@@ -151,8 +173,309 @@ DIR_FileSize = 28
     _br: .res 2, $00
     _clst: .tag CLST
     _add_tmp: .res 4, $00
+    _rootdir16: .res 2, $00
 
 .segment "CODE"
+
+; unsigned char pf_mount()
+; mount a drive
+
+.proc _pf_mount
+    jsr _disk_initialize
+    cmp #STA_NOINIT
+    bne :+
+
+    lda #FR_NOT_READY
+    rts
+
+:   stz _sector
+    stz _sector+1
+    stz _sector+2
+    stz _sector+3
+
+    jsr _check_fs               ; get fat type
+    sta _FatFs+FATFS::fs_type
+    jeq @fat_ok                 ; if 0 - we're OK
+    cmp #$01                    ; if 1 - check MBR
+    bne @check_fs_type          ; otherwise jump to reporting appropriate error
+
+    lda #<MBR_Table             ; read 16 bytes of mbr
+    sta _offset
+    lda #>MBR_Table
+    sta _offset+1
+    lda #$10
+    sta _count
+    jsr _disk_readp
+    beq :+
+
+    lda #$03                    ; if there's a read error, fstype = 3 (error)
+    sta _FatFs+FATFS::fs_type
+    jmp @check_fs_type
+
+:   lda _buff+4                 ; if byte 4 is set, partition exists
+    beq @check_fs_type
+
+    lda _buff+8                 ; get partition offset in LBA
+    sta _sector
+    lda _buff+8+1
+    sta _sector+1
+    lda _buff+8+2
+    sta _sector+2
+    lda _buff+8+3
+    sta _sector+3
+
+    jsr _check_fs
+    sta _FatFs+FATFS::fs_type
+
+@check_fs_type:
+    lda _FatFs+FATFS::fs_type   ; check fs_type
+    cmp #$03                    ; if 3, return error
+    bne :+
+    lda #FR_DISK_ERR
+    rts
+:   cmp #$00                    ; if not 0, return no file system
+    beq @fat_ok
+    lda #FR_NO_FILESYSTEM
+    rts
+
+@fat_ok:
+    lda #$0D
+    sta _offset
+    stz _offset+1
+    lda #$24
+    sta _count
+
+    jsr _disk_readp             ; read file system info
+    beq :+
+    lda #FR_DISK_ERR
+    rts
+
+
+:   lda _buff+BPB_FATSz16-13    ; sectors per FAT(16?)
+    sta _clst                   ; using clst for as temporary space for mult
+    lda _buff+BPB_FATSz16-13+1
+    sta _clst+1
+    stz _clst+2
+    stz _clst+3
+
+    stz _work
+    stz _work+1
+    stz _work+2
+    stz _work+3
+
+    ; work = clst * buff+BPB_NumFATs-13
+    ldy _buff+BPB_NumFATs-13
+:   clc
+    lda _work
+    adc _clst
+    sta _work
+    lda _work+1
+    adc _clst+1
+    sta _work+1
+    lda _work+2
+    adc _clst+2
+    sta _work+2
+    lda _work+3
+    adc _clst+3
+    sta _work+3
+    dey
+    bne :-
+
+    clc
+    lda _sector
+    adc _buff+BPB_RsvdSecCnt-13
+    sta _FatFs+FATFS::fatbase
+    lda _sector+1
+    adc _buff+BPB_RsvdSecCnt-13+1
+    sta _FatFs+FATFS::fatbase+1
+    lda _sector+2
+    adc #$00
+    sta _FatFs+FATFS::fatbase+2
+    lda _sector+3
+    adc #$00
+    sta _FatFs+FATFS::fatbase+3
+
+    lda _buff+BPB_SecPerClus-13 ; sectors per cluster
+    sta _FatFs+FATFS::csize
+
+    lda _buff+BPB_RootEntCnt-13 ; root directory entries
+    sta _FatFs+FATFS::n_rootdir
+    lda _buff+BPB_RootEntCnt-13+1
+    sta _FatFs+FATFS::n_rootdir+1
+
+    lda _buff+BPB_TotSec16-13   ; number of sectors on filesystem (16)
+    sta _clst
+    lda _buff+BPB_TotSec16-13+1
+    sta _clst+1
+    stz _clst+2
+    stz _clst+3
+
+    lda _clst
+    ora _clst+1
+    bne :+
+
+    lda _buff+BPB_TotSec32-13   ; or number of sectors on filesystem (32)
+    sta _clst
+    lda _buff+BPB_TotSec32-13+1
+    sta _clst+1
+    lda _buff+BPB_TotSec32-13+2
+    sta _clst+2
+    lda _buff+BPB_TotSec32-13+3
+    sta _clst+3
+
+:   sec                         ; -= BPB_RsvdSecCnt
+    lda _clst
+    sbc _buff+BPB_RsvdSecCnt-13
+    sta _clst
+    lda _clst+1
+    sbc _buff+BPB_RsvdSecCnt-13+1
+    sta _clst+1
+    lda _clst+2
+    sbc #$00
+    sta _clst+2
+    lda _clst+3
+    sbc #$00
+    sta _clst+3
+
+    sec                         ; -= fsize
+    lda _clst
+    sbc _work
+    sta _clst
+    lda _clst+1
+    sbc _work+1
+    sta _clst+1
+    lda _clst+2
+    sbc _work+2
+    sta _clst+2
+    lda _clst+3
+    sbc _work+3
+    sta _work+3
+
+    lda _FatFs+FATFS::n_rootdir ; store rootdir/16 because in it's own var
+    sta _rootdir16              ; since we need it later
+    lda _FatFs+FATFS::n_rootdir+1
+    sta _rootdir16+1
+
+    lsr _rootdir16+1            ; shift right 4
+    ror _rootdir16
+    lsr _rootdir16+1
+    ror _rootdir16
+    lsr _rootdir16+1
+    ror _rootdir16
+    lsr _rootdir16+1
+    ror _rootdir16
+
+    sec                         ; -= work (ie n_rootdir>>4)
+    lda _clst
+    sbc _rootdir16
+    sta _clst
+    lda _clst+1
+    sbc _rootdir16+1
+    sta _clst+1
+    lda _clst+2
+    sbc #$00
+    sta _clst+2
+    lda _clst+3
+    sbc #$00
+    sta _clst+3
+
+    ; csize is 1, 2, 4, 8, 16, 32, or 128
+    lda _FatFs+FATFS::csize
+    cmp #$01
+    jeq @shift0
+    cmp #$02
+    jeq @shift1
+    cmp #$04
+    jeq @shift2
+    cmp #$08
+    beq @shift3
+    cmp #$10
+    beq @shift4
+    cmp #$20
+    beq @shift5
+    cmp #$40        ; will it really never be 64?
+    beq @shift6
+@shift7:
+    shift_right_32 _clst
+@shift6:
+    shift_right_32 _clst
+@shift5:
+    shift_right_32 _clst
+@shift4:
+    shift_right_32 _clst
+@shift3:
+    shift_right_32 _clst
+@shift2:
+    shift_right_32 _clst
+@shift1:
+    shift_right_32 _clst
+@shift0:
+    clc             ; add 2, but only care about low 2 bytes at this point
+    lda _clst       ; and store in n_fatent
+    adc #$02
+    sta _FatFs+FATFS::n_fatent
+    lda _clst+1
+    adc #$00
+    sta _FatFs+FATFS::n_fatent+1
+
+    lda _FatFs+FATFS::n_fatent+1
+    cmp #$0F
+    bcc @not_fat_16
+    bcs :+
+    lda _FatFs+FATFS::n_fatent
+    cmp #$F8
+    bcc @not_fat_16
+:   lda _FatFs+FATFS::n_fatent+1
+    cmp #$FF
+    bcc @fat_16
+    lda _FatFs+FATFS::n_fatent
+    cmp #$F7
+    bcc @fat_16
+
+@not_fat_16:
+    lda #FR_NO_FILESYSTEM
+    rts
+
+@fat_16:
+    lda #FS_FAT16
+    sta _FatFs+FATFS::fs_type
+
+    clc
+    lda _FatFs+FATFS::fatbase
+    adc _work
+    sta _FatFs+FATFS::dirbase
+    sta _FatFs+FATFS::database
+    lda _FatFs+FATFS::fatbase+1
+    adc _work+1
+    sta _FatFs+FATFS::dirbase+1
+    sta _FatFs+FATFS::database+1
+    lda _FatFs+FATFS::fatbase+2
+    adc _work+2
+    sta _FatFs+FATFS::dirbase+2
+    sta _FatFs+FATFS::database+2
+    lda _FatFs+FATFS::fatbase+3
+    adc _work+3
+    sta _FatFs+FATFS::dirbase+3
+    sta _FatFs+FATFS::database+3
+
+    clc
+    lda _FatFs+FATFS::database
+    adc _rootdir16
+    sta _FatFs+FATFS::database
+    lda _FatFs+FATFS::database+1
+    adc _rootdir16+1
+    sta _FatFs+FATFS::database+1
+    lda _FatFs+FATFS::database+2
+    adc #$00
+    sta _FatFs+FATFS::database+2
+    lda _FatFs+FATFS::database+3
+    adc #$00
+    sta _FatFs+FATFS::database+3
+
+    stz _FatFs+FATFS::flag
+    lda #FR_OK
+    rts
+.endproc
 
 ; unsigned char pf_open()
 ; open a file
